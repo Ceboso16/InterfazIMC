00660b398657387b67cab402a430b8e317ea854b
Anonimous <Anon@ceboso16>
2017-05-10 00:45:15 -0500

    IMC

---
+++ src/imc/IMC.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imc;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Anonimous
+ */
+public class IMC extends JFrame{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Ventana calculadora = new Ventana();
+ 
+    }
+
+    
+}
